5ca2c3310173f288d7117704634f574c468a8008
Mikul Shah <134720598+Pezerin@users.noreply.github.com>
2024-09-01 11:45:03 -0400

    Created recursive fibonacci function

--- fibonacci.js
+++ fibonacci.js
@@ -8,3 +8,16 @@ const fibs = (n) => {
 
   return arr;
 };
+
+// Recursive fibonacci function
+const fibsRec = (n) => {
+  if (n < 2) {
+    return [0];
+  } else if (n === 2) {
+    return [0, 1];
+  } else {
+    let arr = fibsRec(n - 1);
+    arr.push(arr[arr.length - 1] + arr[arr.length - 2]);
+    return arr;
+  }
+};
